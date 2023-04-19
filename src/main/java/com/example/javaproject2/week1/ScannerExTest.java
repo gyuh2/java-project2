550cbf71970ea2da12f27f215389046c960c32b5
gyuh2 <rbgmlrbfl@naver.com>
2023-04-19 16:24:29 +0900

    feat: Scanner Test - ScannerEx, ScannerExTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus(); // read와 plus를 분리 권장
+    }
+}
