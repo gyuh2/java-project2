b1757748a98d6e20257dfd8b06259bd5f529b72b
gyuh2 <rbgmlrbfl@naver.com>
2023-04-20 11:32:58 +0900

    feat: .printf() 포맷팅 - PrintfEx, PrintTwoValuesShortage, MultiplicationTable 추가

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintTwoValuesShortage.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        /* [오류] MissingFormatArgumentException 예외 발생
+          System.out.printf("%s님 안녕하세요, 그런데 제가 하고 싶은 말은 %s입니다.","임규희","");
+        */
+        System.out.printf("%s님 %s!","임규희","안녕하세요");
+    }
+}
