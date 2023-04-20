ae534bb5e0daee2ced605ca1383a57e833f5855c
gyuh2 <rbgmlrbfl@naver.com>
2023-04-20 16:17:16 +0900

    feat: Codeup_2일차 - 1014 ~ 1015 완료

---
+++ src/main/java/com/example/javaproject2/codeup/day4/Codeup1015.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup.day4;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float val1 = sc.nextFloat();
+
+        System.out.printf("%.2f", val1);
+    }
+}
