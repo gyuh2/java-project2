3303fa66031721c6ac52f1a155c20df6b9c7c343
gyuh2 <rbgmlrbfl@naver.com>
2023-04-21 10:54:59 +0900

    feat: Codeup_3일차 - 1017 변수명 수정

--- src/main/java/com/example/javaproject2/codeup/day5/Codeup1017.java
+++ src/main/java/com/example/javaproject2/codeup/day5/Codeup1017.java
@@ -6,7 +6,7 @@ public class Codeup1017 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int val = sc.nextInt();
-        System.out.printf("%d %d %d", val, val, val);
+        int ival = sc.nextInt();
+        System.out.printf("%d %d %d", ival, ival, ival);
     }
 }
