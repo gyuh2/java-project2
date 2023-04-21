2e256f86ab416881f2b84e9f2657c9381314c0ca
gyuh2 <rbgmlrbfl@naver.com>
2023-04-22 01:35:25 +0900

    feat: PrintWhy 추가 - print() 메소드 호출 불가능한 불합리한 구조 실습

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintWhy.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1.day4;
+// 불합리한 구조
+public class PrintWhy {
+    void print() {
+        System.out.println("WHy");
+    }
+
+    public static void main(String[] args) {
+        //print() ;   --> 불가능
+    }
+}
