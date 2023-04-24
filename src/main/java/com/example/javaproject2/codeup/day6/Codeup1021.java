ce3dbd894de6101c0bc7001dcf85cf44769a487b
gyuh2 <rbgmlrbfl@naver.com>
2023-04-24 15:41:55 +0900

    feat: CodeUp_4일차 - 1021 ~ 1023, 1028 ~ 1030 완료

---
+++ src/main/java/com/example/javaproject2/codeup/day6/Codeup1021.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup.day6;
+
+import java.util.Scanner;
+
+public class Codeup1021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sVal = sc.next();
+        System.out.printf("%s", sVal);
+    }
+}
