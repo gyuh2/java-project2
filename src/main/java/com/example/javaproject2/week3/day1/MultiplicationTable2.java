f54f3e8fcfd11dab531c6cd1b1c8f31f5ccd8871
gyuh2 <rbgmlrbfl@naver.com>
2023-05-01 14:53:52 +0900

    feat: 특정 단 선택 출력 - MultiplicationTable2

---
+++ src/main/java/com/example/javaproject2/week3/day1/MultiplicationTable2.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week3.day1;
+
+public class MultiplicationTable2 {
+    public static void main(String[] args) {
+        int[] dan = {2, 4, 6, 8};
+
+        for (int i = 0; i < dan.length; i++) {
+            for (int j = 1; j < 9; j++) {
+                System.out.printf("%d * %d = %d\n", dan[i], j, dan[i] * j);
+            }
+            System.out.println("----------");
+        }
+    }
+}
