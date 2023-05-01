2b489e3d3f0d4ae96a39451870948ee67bf84e5e
gyuh2 <rbgmlrbfl@naver.com>
2023-05-01 13:46:33 +0900

    feat: for문 유니코드 출력 - ForLoopUni 추가

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopUni.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopUni {
+    public static void main(String[] args) {
+        for (char c = '가'; c <= '힣' ; c++) {
+            System.out.printf("%c ", c);
+
+        }
+    }
+}
