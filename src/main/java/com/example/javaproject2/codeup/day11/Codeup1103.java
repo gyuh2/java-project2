077b7c51067bc193fffe48ec04bef3fffba760d8
gyuh2 <rbgmlrbfl@naver.com>
2023-05-01 09:17:33 +0900

    feat: CodeUp_11일차 - 1103, 1106 완료

---
+++ src/main/java/com/example/javaproject2/codeup/day11/Codeup1103.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup.day11;
+
+public class Codeup1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/day11/Codeup1106.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.codeup.day11;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        System.out.printf("%d %d\n", min, max);
+    }
+}
