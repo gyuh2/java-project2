f0369876b2bbf4a2c311806d7730068ca5499702
gyuh2 <rbgmlrbfl@naver.com>
2023-05-01 10:55:20 +0900

    feat: CodeUp_11일차 - 1072 완료

---
+++ src/main/java/com/example/javaproject2/codeup/day11/Codeup1072.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup.day11;
+
+import java.util.Scanner;
+
+public class Codeup1072 {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for (int i = 0; i < input; i++) {
+            System.out.println(sc.nextInt());
+        }
+    }
+}
