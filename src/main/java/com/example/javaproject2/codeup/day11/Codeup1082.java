660ce275adeb6febc79429d5639e51d282f1dfa6
gyuh2 <rbgmlrbfl@naver.com>
2023-05-02 09:47:03 +0900

    feat: CodeUp_12일차 - 1082 주석 추가

--- src/main/java/com/example/javaproject2/codeup/day11/Codeup1082.java
+++ src/main/java/com/example/javaproject2/codeup/day11/Codeup1082.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Codeup1082 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hexVal = Integer.parseInt(sc.next(), 16);
+        int hexVal = Integer.parseInt(sc.next(), 16); // 입력 받는 값 16진수 취급
 
         for (int i = 1; i < 16 ; i++) {
             System.out.printf("%X*%X=%X\n",hexVal,i, hexVal*i);
