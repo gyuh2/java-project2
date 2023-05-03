1c67543da507f72c01f3f88cd5cd23d84042c222
gyuh2 <rbgmlrbfl@naver.com>
2023-05-03 11:09:10 +0900

    feat: 약수 구하기 - Factor 추가

---
+++ src/main/java/com/example/javaproject2/week3/day3/Factor.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week3.day3;
+
+public class Factor {
+    public static void main(String[] args) {
+        int num = 7;
+        // 나머지 0 == 나누어 떨어짐
+        for (int i = 1; i <= num; i++) {
+            System.out.printf("%d %% %d --> %d\n", num, i, num % i);
+        }
+    }
+}
