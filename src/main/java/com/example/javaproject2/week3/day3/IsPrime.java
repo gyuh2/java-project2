6494e67b00cfb31d7b7cd32b14a5b0819e880d58
gyuh2 <rbgmlrbfl@naver.com>
2023-05-03 12:54:20 +0900

    feat: 소수 판별하기 - IsPrime 추가

---
+++ src/main/java/com/example/javaproject2/week3/day3/IsPrime.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week3.day3;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        int num = 6;
+        int factors = 0; // 약수의 개수
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) factors++;
+        }
+        // factors = 1
+        System.out.printf("factors: %d\n", factors);
+        System.out.println(factors == 0 ? num + "은(는) 소수입니다" : num + "은(는) 소수가 아닙니다");
+    }
+}
