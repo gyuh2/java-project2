d3e5335b1a39a85d3e6e5bb5e579875a294b9f56
gyuh2 <rbgmlrbfl@naver.com>
2023-05-03 11:10:54 +0900

    feat: CodeUp_13일 - 1273 완료(약수 구하기)

---
+++ src/main/java/com/example/javaproject2/codeup/day13/Codeup1273.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup.day13;
+
+import java.util.Scanner;
+
+public class Codeup1273 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num ; i++) {
+            if (num % i == 0) System.out.printf("%d ", i);
+        }
+    }
+}
