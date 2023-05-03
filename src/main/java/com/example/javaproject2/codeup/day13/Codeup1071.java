eb0815d00a9686746975f14fb33c6e43c078977c
gyuh2 <rbgmlrbfl@naver.com>
2023-05-03 10:15:03 +0900

    feat: CodeUp_13일 - 1071, 1073 완료

---
+++ src/main/java/com/example/javaproject2/codeup/day13/Codeup1071.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup.day13;
+
+import java.util.Scanner;
+
+public class Codeup1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        do {
+            num = sc.nextInt();
+            if (num == 0) break;
+            System.out.println(num);
+        } while(num != 0);
+    }
+}
