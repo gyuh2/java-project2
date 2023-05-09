1a9abd377632f70b03c94dabaecb7148d7614399
gyuh2 <rbgmlrbfl@naver.com>
2023-05-09 09:02:08 +0900

    feat: StringArgs 실습 - StringArgs

--- src/main/java/com/example/javaproject2/week4/StringArgs.java
+++ src/main/java/com/example/javaproject2/week4/StringArgs.java
@@ -2,7 +2,7 @@ package com.example.javaproject2.week4;
 
 import java.util.Arrays;
 
-public class ArgsPrinter {
+public class StringArgs {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
     }
